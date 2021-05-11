8d058f62b806d3af8f586e54f88bf25066552d68
PabloBiasco74946 <81331021+PabloBiasco74946@users.noreply.github.com>
2021-05-11 18:57:56 -0300

    Tarea 6
    Clase 6

---
+++ ProyectoBase/ProyectoBase/AccesoADatos/AD_Personas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoBase.AccesoADatos
+{
+    class AD_Personas
+    {
+        public static DataTable ObtenerListadoPersonasReducido()
+        {
+
+
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                bool resultado = false;
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT Nombre, Apellido, IdTipoDocumento, NumeroDocumento FROM personas";
+
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+                return tabla;
+                if (tabla.Rows.Count == 1)
+                {
+                    resultado = true;
+                }
+
+                else
+                {
+                    resultado = false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+    }
+}
